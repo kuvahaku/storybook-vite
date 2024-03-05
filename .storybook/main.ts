5f0da905263141358f295ee8b83a2c4d765a571a
kuvahaku <anonymous@example.com>
2024-03-05 11:46:41 +0100

    Your relevant commit message

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,10 +3,12 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
+    "@chromatic-com/storybook",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm"
   ],
   framework: {
     name: "@storybook/react-vite",
